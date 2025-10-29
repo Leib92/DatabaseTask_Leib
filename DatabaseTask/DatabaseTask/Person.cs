ffa31d2b192358f3339a5b970b3f931910e2040e
Leib92 <opilane@HP-CZC0359NNL>
2025-10-29 14:04:09 +0200

    Succesful Code Migration

---
+++ DatabaseTask/DatabaseTask/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DatabaseTask;
+
+public partial class Person
+{
+    public int Id { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public string? Address { get; set; }
+}
---
+++ DatabaseTask/DatabaseTask/PersonDatabaseContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseTask;
+
+public partial class PersonDatabaseContext : DbContext
+{
+    public PersonDatabaseContext()
+    {
+    }
+
+    public PersonDatabaseContext(DbContextOptions<PersonDatabaseContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Person> People { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=ConnectionStrings:DefaultConnection");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Person>(entity =>
+        {
+            entity
+                .HasNoKey()
+                .ToTable("Person");
+
+            entity.Property(e => e.Address)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("address");
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("first_name");
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.LastName)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("last_name");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
